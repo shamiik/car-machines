2af0ece1e90356d36661c70687466e7148aa112b
shamiik <shamikdey7@gmail.com>
2021-11-12 04:53:38 +0600

    notfound added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
 
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
